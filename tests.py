783945a87cfe2bcd6df2f57fc395e304961fa6f1
Ilya Kulakov <kulakov.ilya@gmail.com>
2012-11-12 15:13:46 +0700

    tests can be run as a usual script.

--- tests.py
+++ tests.py
@@ -31,3 +31,16 @@ class TestObserver(power.PowerManagementObserver):
 
     def on_time_remaining_change(self, power_management):
         print "on_time_remaining_change"
+
+
+if __name__ == "__main__":
+    o = TestObserver()
+    p = power.PowerManagement()
+    p.add_observer(o)
+    try:
+        print "Power management observer is registered"
+        import time
+        while True:
+            time.sleep(1)
+    finally:
+        p.remove_observer(o)
