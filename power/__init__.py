7d8372a5e5bc792569be9680469d4467ac9cc6b7
Ilya Kulakov <kulakov.ilya@gmail.com>
2012-11-14 00:59:12 +0700

    Instead of exception show a warning and return no-op PowerManagement.

--- power/__init__.py
+++ power/__init__.py
@@ -12,4 +12,47 @@ elif platform.startswith('win32'):
 elif platform.startswith('linux'):
     from linux import PowerManagement
 else:
-    raise NotImplementedError("%s is not supported. If you believe it should be supported, check the '%s'.".format(platform, __file__))
+    from common import PowerManagementBase
+    import warnings
+    class PowerManagement(PowerManagementBase):
+        """
+        No-op subclass of PowerManagement.
+        It operates like AC is always attached and power sources are never changed.
+        """
+        def get_providing_power_source_type(self):
+            """
+            @return: Always POWER_TYPE_AC
+            """
+            return POWER_TYPE_AC
+
+        def get_low_battery_warning_level(self):
+            """
+            @return: Always LOW_BATTERY_WARNING_NONE
+            """
+            return LOW_BATTERY_WARNING_NONE
+
+        def get_time_remaining_estimate(self):
+            """
+            @return: Always TIME_REMAINING_UNLIMITED
+            """
+            return TIME_REMAINING_UNLIMITED
+
+        def add_observer(self, observer):
+            """
+            Does nothing.
+            """
+            pass
+
+        def remove_observer(self, observer):
+            """
+            Does nothing.
+            """
+            pass
+
+        def remove_all_observers(self):
+            """
+            Does nothing.
+            """
+            pass
+
+    warnings.warn("{platform} is not supported. No-op PowerManagement class is used.".format(platform=platform))
